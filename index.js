0dba4f3f496116a49b1527e4f71edf4bdf0ed023
stuard19 <74276909+stuard19@users.noreply.github.com>
2021-08-17 17:18:55 +0700

    Create index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://scythe-energetic-munchkin.glitch.me"]
+const urls = ["https://bot-cavendish.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
